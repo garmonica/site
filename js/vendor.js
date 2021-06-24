ca15ca621ee2a8f3c94ba8e7f0d5de6540c311e9
Olia <good-friday@mail.ru>
2021-06-24 21:13:17 +0300

    Added pin icon

--- js/vendor.js
+++ js/vendor.js
@@ -1,5 +1,7 @@
 /* global L:readonly */
 
+const TEXT = 'Санкт-Петербург – русский портовый город на побережье Балтийского моря, который в течение двух веков служил столицей Российской империи. Он был основан в 1703 году Петром I, которому воздвигнут знаменитый памятник "Медный всадник". Город по праву считается культурным центром страны. У туристов пользуются популярностью Мариинский театр, где проходят оперные и балетные спектакли, и Государственный Русский музей с коллекцией русского искусства, которая включает как православные иконы, так и работы художника-абстракциониста Василия Кандинского.';
+
 const map = L.map('map')
   .setView({
     lat: 59.93918,
@@ -14,7 +16,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg',
+  iconUrl: '../img/icon-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -30,4 +32,6 @@ const mainPinMarker = L.marker(
   },
 );
 
-mainPinMarker.addTo(map);
+mainPinMarker
+  .addTo(map)
+  .bindPopup(TEXT);
